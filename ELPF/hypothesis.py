602a5849dc2cffda4d272fe91b8f72d6c688fd93
Joshua Wakefield <j.wakefield359@gmail.com>
2024-11-11 15:59:57 +0000

    Add wrapper classes for hypothesis types

---
+++ ELPF/hypothesis.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+
+class Hypothesis:
+
+    def __init__(self, prediction, measurement, measurement_prediction=None):
+        self.prediction = prediction
+        self.measurement = measurement
+        self.measurement_prediction = measurement_prediction
+
+
+class SingleHypothesis(Hypothesis):
+
+    def __init__(self, prediction, measurement, probability, measurement_prediction=None):
+        super().__init__(prediction, measurement, measurement_prediction)
+        self.probability = probability
+
+
+class JointHypothesis:
+
+    def __init__(self, hypotheses):
+        self.hypotheses = hypotheses
+        self.probability = np.prod([h.probability for h in hypotheses], axis=0)
