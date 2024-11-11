a17738e9acb03285a0d559499a84974798dff5e6
Joshua Wakefield <j.wakefield359@gmail.com>
2024-11-11 15:59:36 +0000

    Add wrapper classes for detection types

--- ELPF/detection.py
+++ ELPF/detection.py
@@ -1,6 +1,6 @@
 class Detection:
 
-    def __init__(self, state_vector, measurement_model, timestamp=None):
+    def __init__(self, state_vector=None, measurement_model=None, timestamp=None):
         self.state_vector = state_vector
         self.measurement_model = measurement_model
         self.timestamp = timestamp
@@ -8,6 +8,10 @@ class Detection:
 
 class TrueDetection(Detection):
     """Class representing a true detection in the measurement space."""
+
+
+class MissedDetection(Detection):
+    """Class representing a missed detection in the measurement space."""
 
 
 class Clutter(Detection):
